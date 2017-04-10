b714d72b1230581a7356227e8a6eb1746cb62d0e
BelenMayo <belenmaydi@hotmail.com>
2017-04-09 23:06:02 -0300

    PPT - Primer commit
    App

--- Trivia2017/src/app/app.module.ts
+++ Trivia2017/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { Vibration } from '@ionic-native/vibration';
+import { Flashlight } from '@ionic-native/flashlight';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -10,6 +12,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 @NgModule({
   declarations: [
@@ -34,6 +37,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Vibration,
+    Flashlight,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- Trivia2017/src/pages/home/home.ts
+++ Trivia2017/src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Vibration } from '@ionic-native/vibration';
+import { Flashlight } from '@ionic-native/flashlight';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public vibration: Vibration, public flashlight: Flashlight) {
 
+  }
+
+  public HacerVibrar()
+  {
+    this.vibration.vibrate(3000);
+  }
+
+  public Parar()
+  {
+    this.vibration.vibrate(0);
+  }
+
+
+    public PrenderLinterna()
+  {
+    this.flashlight.switchOn();
+  }
+
+  public ApagarLinterna()
+  {
+    this.flashlight.switchOff();
   }
 
 }
