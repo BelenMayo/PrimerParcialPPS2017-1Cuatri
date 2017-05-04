abbfe35058837ff0f3432bb428223d9434a15970
BelenMayo <belenmaydi@hotmail.com>
2017-05-04 11:32:32 -0300

    Sonido Piano
    Sonido Piano

--- Piano2017/src/pages/piano/piano.ts
+++ Piano2017/src/pages/piano/piano.ts
@@ -12,14 +12,40 @@ import * as $ from 'jquery'
 })
 export class Piano {
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController, private vibration: Vibration) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, private vibration: Vibration,
+    private nativeAudio: NativeAudio) {
+      this.nativeAudio.preloadSimple('caballo', 'assets/sound/caballo.mp3');
+      this.nativeAudio.preloadSimple('vaca', 'assets/sound/vaca.mp3');
+      this.nativeAudio.preloadSimple('oveja', 'assets/sound/oveja.mp3');
+      this.nativeAudio.preloadSimple('cerdo', 'assets/sound/cerdo.mp3');
+      this.nativeAudio.preloadSimple('gallo', 'assets/sound/gallo.mp3');
+      this.nativeAudio.preloadSimple('pato', 'assets/sound/pato.mp3');
   }
 
-  pasarPregunta(valor){
+  reproducirSonido(valor){
 
-  if(valor != "perro"){
-    this.vibration.vibrate([200,100,200]);
-  } else {
+  if(valor == "Caballo"){
+    this.nativeAudio.play('caballo', () => console.log("Caballo"));
+    this.vibration.vibrate(300);
+  }
+  if(valor == "Vaca"){
+    this.nativeAudio.play('vaca', () => console.log("Vaca"));
+    this.vibration.vibrate(300);
+  }
+  if(valor == "Oveja"){
+    this.nativeAudio.play('oveja', () => console.log("Oveja"));
+    this.vibration.vibrate(300);
+  }
+  if(valor == "Cerdo"){
+    this.nativeAudio.play('cerdo', () => console.log("Cerdo"));
+    this.vibration.vibrate(300);
+  }
+  if(valor == "Gallo"){
+    this.nativeAudio.play('gallo', () => console.log("Gallo"));
+    this.vibration.vibrate(300);
+  }
+  if(valor == "Pato"){
+    this.nativeAudio.play('pato', () => console.log("Pato"));
     this.vibration.vibrate(300);
   }
 
